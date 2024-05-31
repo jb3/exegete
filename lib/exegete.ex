a35da74b35a4132f6a0a93a6a9ee0472ffefe140
Joe Banks <joe@jb3.dev>
2024-05-31 04:30:54 +0100

    Remove mix boilerplate module contents

--- lib/exegete.ex
+++ lib/exegete.ex
@@ -1,18 +1,2 @@
 defmodule Exegete do
-  @moduledoc """
-  Documentation for `Exegete`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Exegete.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
